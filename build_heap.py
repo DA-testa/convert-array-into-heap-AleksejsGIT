94cdf433d0e1b69444b144b4896fdb67c0ef5ee6
Aleksejs Šepeļevs <118129406+AleksejsGIT@users.noreply.github.com>
2023-03-11 22:58:51 +0200

    Update build_heap.py

--- build_heap.py
+++ build_heap.py
@@ -4,39 +4,48 @@
 
 def build_heap(data):
     swaps = []
-    # TODO: Creat heap and heap sort
-    # try to achieve  O(n) and not O(n2)
-
-
+    tests = len(data)
+    for i in range(tests//2, -1, -1):
+        while(True):
+            min_index = i
+            first = 2*i+1
+            if first < data and data[first] < data[min_index]:
+                min_index = first
+            second = 2*i+2
+            if second < data and data[second] < data[min_index]:
+                min_index = second
+            if i != min_index:
+                swaps.append(i, min_index)
+                data[i], data[min_index] = data[min_index], data[i]
+                i = min_index
+                continue
+            break
     return swaps
 
 
 def main():
-    
-    # TODO : add input and corresponding checks
-    # add another input for I or F 
-    # first two tests are from keyboard, third test is from a file
 
+    ievade = input()
+    if "I" in ievade:
+        n = int(input())
+        data = list(map(int, input().split()))
+    elif "F" in ievade:
+        ievade2 = input()
+        if "a" in ievade2:
+            return()
+        with open ("tests/" + ievade2) as file:
+            n = int(file.readline())
+            data = list(map(int, file.readline().split()))
+    else:
+        return()
 
-    # input from keyboard
-    n = int(input())
-    data = list(map(int, input().split()))
-
-    # checks if lenght of data is the same as the said lenght
     assert len(data) == n
 
-    # calls function to assess the data 
-    # and give back all swaps
     swaps = build_heap(data)
-
-    # TODO: output how many swaps were made, 
-    # this number should be less than 4n (less than 4*len(data))
-
-
-    # output all swaps
     print(len(swaps))
     for i, j in swaps:
         print(i, j)
+
 
 
 if __name__ == "__main__":
